0301192f0aedccd8a6076f611b28c18354772877
giorgio <gbiondillo@tecnoscientia.com>
2021-07-08 11:01:21 +0200

    perfetto

--- create.js
+++ create.js
@@ -1,135 +1,49 @@
-// const sqlite3 = require('sqlite3').verbose();
-// const db = new sqlite3.Database('users.db');
-
-// const initDb = `CREATE TABLE IF NOT EXISTS users(
-//     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-//     NAME        CHAR(255)       NOT NULL,
-//     AGE         INT             NOT NULL,
-//     SURNAME     CHAR(255),
-//     EMAIL       CHAR(255)
-//  );`
-
-// db.run(initDb);
-
-// const users = [
-//     {
-//         name: "Pippo",
-//         surname: "Baudo",
-//         age: 25,
-//         email: "pippobaudo@example.com"
-//     },
-//     {
-//         name: "Francesco",
-//         surname: "Sole",
-//         age: 30,
-//         email: "franci@example.com"
-//     },
-//     {
-//         name: "Luca",
-//         surname: "Abete",
-//         age: 45,
-//         email: "albero77@example.com"
-//     },
-//     {
-//         name: "Pino",
-//         surname: "Rospo",
-//         age: 13,
-//         email: "pinopino@example.com"
-//     }
-// ];
-
-// const stmt = db.prepare(`INSERT INTO users 
-//     (NAME,SURNAME,EMAIL,AGE) 
-//     VALUES
-//     (?, ?, ?, ?)
-// `)
-
-// users.forEach(el => stmt.run(el.name, el.surname, el.email, el.age))
-
-// //db.close();
-
-
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('persone.db');
+const db = new sqlite3.Database('users.db');
 
-const initDb = `CREATE TABLE IF NOT EXISTS persone(
+const initDb = `CREATE TABLE IF NOT EXISTS users(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     NAME        CHAR(255)       NOT NULL,
-    IDP         INT             
+    AGE         INT             NOT NULL,
+    SURNAME     CHAR(255),
+    EMAIL       CHAR(255)
  );`
 
 db.run(initDb);
 
 const users = [
     {
-        name: "Giorgio",
+        name: "Pippo",
+        surname: "Baudo",
+        age: 25,
+        email: "pippobaudo@example.com"
     },
     {
-        name: "Paolo",
-        idp: 1
+        name: "Francesco",
+        surname: "Sole",
+        age: 30,
+        email: "franci@example.com"
     },
     {
         name: "Luca",
-        idp: 5
+        surname: "Abete",
+        age: 45,
+        email: "albero77@example.com"
     },
     {
-        name: "Pippo",
-        idp: 1
-    },
-    {
-        name: "Pluto",
-    },
-    {
-        name: "Beppe",
-        idp: 5
-    },
-    {
-        name: "Franco",
-        idp: 5
-    },
-    {
-        name: "Lorenzo",
-        idp: 1
-    },
-    {
-        name: "Checco",
-        idp: 5
-    },
-    {
-        name: "Lino",
-        idp: 1
+        name: "Pino",
+        surname: "Rospo",
+        age: 13,
+        email: "pinopino@example.com"
     }
-    // {
-    //     name: "Pippo",
-    //     surname: "Baudo",
-    //     age: 25,
-    //     email: "pippobaudo@example.com"
-    // },
-    // {
-    //     name: "Francesco",
-    //     surname: "Sole",
-    //     age: 30,
-    //     email: "franci@example.com"
-    // },
-    // {
-    //     name: "Luca",
-    //     surname: "Abete",
-    //     age: 45,
-    //     email: "albero77@example.com"
-    // },
-    // {
-    //     name: "Pino",
-    //     surname: "Rospo",
-    //     age: 13,
-    //     email: "pinopino@example.com"
-    // }
 ];
 
-const stmt = db.prepare(`INSERT INTO persone 
-    (NAME,IDP) 
+const stmt = db.prepare(`INSERT INTO users 
+    (NAME,SURNAME,EMAIL,AGE) 
     VALUES
-    (?, ?)`)
+    (?, ?, ?, ?)
+`)
 
-users.forEach(el => stmt.run(el.name, el.idp))
+users.forEach(el => stmt.run(el.name, el.surname, el.email, el.age))
 
 //db.close();
